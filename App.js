34577bb59e08e73f8130bf6d53ea5ecd9468a919
mtahabahsi <m.tahabahsi@gmail.com>
2020-09-30 19:01:30 +0300

    ilk ekleme

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput, SafeAreaView } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Some text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{
+            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+          }}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput
+        style={{
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1
+        }}
+        defaultValue="You can type in me"
+      />
+    </SafeAreaView>
+
+  );
+}
+
+export default App;
